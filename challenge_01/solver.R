ac9c61ab30441387ef8488426c22361a2fac7449
katiesaund <katiemsaund@gmail.com>
2020-12-01 19:28:34 -0500

    Solve problem

---
+++ challenge_01/solver.R
@@ -0,0 +1,35 @@
+# Goal: Find the two entries that sum to 2020 and then multiply those two 
+# numbers together. Return the answer. 
+
+library(tidyverse)
+
+# First, let's read in the puzzle input and then check some assumptions. 
+
+num_df <- read_csv("puzzle_input.csv", col_names = FALSE)
+colnames(num_df) <- "value"
+# Are they all positive numbers less than 2020? 
+
+bad_values <- num_df %>% 
+  select(value) %>% 
+  filter(value > 2020,
+         value < 1, 
+         !is.double(value)) %>% 
+  pull
+is_empty(bad_values) # TRUE therefore Empty
+
+# Easy solution is to run two for loops.
+
+num_value <- nrow(num_df)
+
+for (i in 1:num_value) {
+  for (j in 1:num_value) {
+    num1 <- num_df$value[i] 
+    num2 <- num_df$value[j]
+    if (num1 + num2 == 2020) {
+      print(paste(num1, num2))
+      print(num1 * num2)
+    }
+  }
+}
+
+# What's a more elegant way? 
